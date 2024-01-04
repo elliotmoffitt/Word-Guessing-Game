0d372b0ea142b3ba0dcb0bcd2f337cefed1c7309
Elliot <elliot.winton@gmail.com>
2024-01-04 18:48:52 -0500

    module added

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-//asdf
+const readline = require('readline');
+const wordList = require('./wordRepository')
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+rl.close()
+
+console.log(wordList)
