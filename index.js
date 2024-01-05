96165a28ada1f371fb0f3473332c37a4ec9edd40
Elliot <elliot.winton@gmail.com>
2024-01-04 20:49:11 -0500

    asdf

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const randomWord = require('./wordRepository');
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
-})
+});
 
 
 let attempts = 0;
@@ -21,10 +21,30 @@ let compareLetters = () => {
         console.log(answer)
     })
 }
+let currentState = () => {
+    if (guess !== word) {
+        rl.question('Guess a letter: ', answer => {
+            //if user puts in more than one letter or a number, call again
+            if (answer.length > 1 || answer.match(/[(0-9)]/)) {
+                currentState();
+            }
+            //if user puts in uppercase letter, toLowerCase()
+            if (answer.match(/([A-Z])/)) {
+                guess += answer.toLowerCase();
+                console.log(guess)
+                currentState()
+            } else {
+                guess += answer;
+                console.log(guess)
+                currentState()
+            }
 
+        })
+    }
+
+}
+
+// rl.close()
 //if letter user puts in is included in word, .indexOf() where that letter is, replace '_' with letter
 
-
-
-
-console.log(compareLetters())
+currentState()
