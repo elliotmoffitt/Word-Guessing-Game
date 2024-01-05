ffe922f3c4bad947616b0bb90bbb6deadfa4553f
Elliot <elliot.winton@gmail.com>
2024-01-04 19:42:33 -0500

    asdf

--- index.js
+++ index.js
@@ -6,8 +6,33 @@ const rl = readline.createInterface({
     output: process.stdout
 })
 
-rl.close()
+
+let attempts = 0;
+let word = randomWord;
+let guessed = '';
+
+let currentState = () => {
+    rl.question('Guess a letter: ', answer => {
+        //if user puts in more than one letter or a number, call again
+        if (answer.length > 1 || answer.match(/[(0-9)]/)) {
+            currentState();
+        }
+        //if user puts in uppercase letter, toLowerCase()
+        if (answer.match(/([A-Z])/)) {
+            guessed += answer.toLowerCase();
+            currentState()
+            console.log(guessed)
+        } else {
+            guessed += answer;
+            currentState()
+            console.log(guessed)
+        }
+
+    })
+}
+
+// rl.close()
 
 
 
-console.log(randomWord)
+console.log(currentState())
